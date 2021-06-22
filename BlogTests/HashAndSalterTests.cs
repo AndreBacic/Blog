81fa24f0623852343f705fe2ca3991c41cc09377
andre9050@yahoo.com <andre9050@yahoo.com>
2021-06-21 23:34:25 -0400

    More unit tests

---
+++ BlogTests/HashAndSalterTests.cs
@@ -0,0 +1,32 @@
+﻿using BlogDataLibrary.Security;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BlogDataLibrary.Tests
+{
+    public class HashAndSalterTests
+    {
+        [Fact]
+        public void HashAndSalt_ShouldWork()
+        {
+            // Arrange
+            string password = "Test";
+            // Act
+            var model = HashAndSalter.HashAndSalt(password);
+            // Assert
+            Assert.NotNull(model);
+            Assert.True(model.PasswordHashString.Length == 44);
+            Assert.Equal(HashAndSalter.Iterations, model.IterationsOnHash);
+        }
+        [Fact]
+        public void HashAndSalt_ShouldFail()
+        {
+            // Arrange
+            string password = "";
+            // Act/assert
+            Assert.ThrowsAny<Exception>(() => HashAndSalter.HashAndSalt(password));
+        }
+    }
+}
