6a09841354f1d83aa1672c099500ccf9f264a6dd
andre9050@yahoo.com <andre9050@yahoo.com>
2021-06-24 23:59:13 -0400

    Added some basic css and html and refactored js a bit

---
+++ BlogAPI/wwwroot/js/experiments.js
@@ -0,0 +1,36 @@
+﻿function GetAllArticles() {
+    fetch(articleURI, {
+        method: 'GET'
+    })
+        .then(response => response.json())
+        .then(function (json) {
+            articles = json
+            DisplayArticles(json)
+        })
+        .catch(error => console.error('Unable to get articles.', error));
+}
+
+async function GetAllArticlesAsync() {
+    let infoPromise = await fetch(articleURI, { method: 'GET' })
+    articles = await infoPromise.json()
+    DisplayArticles(articles)
+    document.body.append("<h3>Footer?</h3>")
+}
+
+function DisplayArticles(json) {
+    //articles = JSON.parse(json)
+    let html = "<h2>My Articles:</h2>"
+    articles.forEach(article => {
+        html += "<p>" + article.Title + "," + article.AuthorName + " | " + article + "</p>"
+    })
+    document.getElementById("articleList").innerHTML = html;
+}
+
+function DisplayRawArticles(json) {
+    articles = json
+    let html = "<h2>My Articles:</h2>"
+    for (article in articles) {
+        html += "<p>" + article + "</p>"
+    }
+    document.getElementById("articleList").innerHTML = html;
+}
--- BlogAPI/wwwroot/js/site.js
+++ BlogAPI/wwwroot/js/site.js
@@ -3,39 +3,14 @@ const commentURI = "api/CommentApi"
 
 let articles = []
 
-function GetAllArticles() {
-    fetch(articleURI, {
-        method: 'GET'
-    })
-        .then(response => response.json())
-        .then(function (json) {
-            articles = json
-            DisplayArticles(json)
-        })    
-            .catch(error => console.error('Unable to get articles.', error));
-}
-
 async function GetAllArticlesAsync() {
     let infoPromise = await fetch(articleURI, { method: 'GET' })
-    articles = await infoPromise.json()
-    DisplayArticles(articles)
-    document.body.append("<h3>Footer?</h3>")
+    let articles = await infoPromise.json()
+    return articles;
 }
 
-function DisplayArticles(json) {
-    //articles = JSON.parse(json)
-    let html = "<h2>My Articles:</h2>"
-    articles.forEach(article => {
-        html += "<p>" + article.Title + "," + article.AuthorName + " | "  + article + "</p>"
-    })
-    document.getElementById("articleList").innerHTML = html;
-}
-
-function DisplayRawArticles(json) {
-    articles = json
-    let html = "<h2>My Articles:</h2>"
-    for (article in articles) {
-        html += "<p>"+article+"</p>"
-    }
-    document.getElementById("articleList").innerHTML = html;
+async function GetArticleByIdAsync(id) {
+    let infoPromise = await fetch(`${articleURI}/${id}`, { method: 'GET' })
+    let article = await infoPromise.json()
+    return article;
 }
