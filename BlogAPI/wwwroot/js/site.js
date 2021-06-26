19ab1a47914406045176e7178d9e3e02459a130d
andre9050@yahoo.com <andre9050@yahoo.com>
2021-06-25 23:06:36 -0400

    Successfully retrieved data from api back end and displayed it to page

--- BlogAPI/wwwroot/js/site.js
+++ BlogAPI/wwwroot/js/site.js
@@ -14,3 +14,37 @@ async function GetArticleByIdAsync(id) {
     let article = await infoPromise.json()
     return article;
 }
+
+async function RenderArticles() {
+    articles = await GetAllArticlesAsync()
+    let articleList = document.getElementById("articleList")
+    while (articleList.firstChild) {
+        articleList.removeChild(articleList.firstChild)
+    }
+
+    for (let a of articles) {
+        const newArticle = RenderPreviewOfArticle(a)
+        articleList.appendChild(newArticle)
+    }    
+}
+
+function RenderPreviewOfArticle(articleJSON) {
+    const article = document.createElement("article")
+    article.className = "flex-item"
+
+    const header = document.createElement("h4")
+    header.textContent = articleJSON.title
+    article.appendChild(header)
+
+    const content = document.createElement("p")
+    content.textContent = articleJSON.contentText
+    article.appendChild(content)
+
+    const authorNameLabel = document.createElement("p")
+    const citedAuthorName = document.createElement("cite")
+    citedAuthorName.textContent = articleJSON.contentText
+    authorNameLabel.appendChild(citedAuthorName)
+    article.appendChild(authorNameLabel)
+
+    return article
+}
