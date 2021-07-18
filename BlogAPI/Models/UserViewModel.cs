c76b58a297b86b7bb486e51b548aa69a98241149
andre9050@yahoo.com <andre9050@yahoo.com>
2021-07-17 23:59:08 -0400

    css changes, primarily

--- BlogAPI/Models/UserViewModel.cs
+++ BlogAPI/Models/UserViewModel.cs
@@ -11,7 +11,8 @@ namespace BlogAPI.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string EmailAddress { get; set; }
-        
+        public bool DoesReceiveNotifications { get; set; }
+
         public string Name
         {
             get
