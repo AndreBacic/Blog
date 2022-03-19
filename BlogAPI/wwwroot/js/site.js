c34b092916d26de873a40f9ce30625bda3ef94e5
andre9050@yahoo.com <andre9050@yahoo.com>
2022-03-18 22:32:25 -0400

    Final touches

--- BlogAPI/wwwroot/js/site.js
+++ BlogAPI/wwwroot/js/site.js
@@ -17,6 +17,7 @@ const passwordRegEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/g
 const emailRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/g
 
 function isValidEmail(email) {
+    email = email.toLowerCase()
     regexed = email.match(emailRegex)
     if (!regexed) {return false}
     if (regexed.length > 1) {return false}
