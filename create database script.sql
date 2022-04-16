66d7503c66b8e564a080a7f4ca8de13de812bd52
andre9050@yahoo.com <andre9050@yahoo.com>
2022-04-15 22:42:53 -0400

    Saving sql code to git

---
+++ create database script.sql
@@ -0,0 +1,662 @@
+USE [master]
+GO
+/****** Object:  Database [Blog]    Script Date: 4/15/2022 10:38:02 PM ******/
+CREATE DATABASE [Blog]
+ CONTAINMENT = NONE
+ ON  PRIMARY 
+( NAME = N'Blog', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Blog.mdf' , SIZE = 8192KB , MAXSIZE = UNLIMITED, FILEGROWTH = 65536KB )
+ LOG ON 
+( NAME = N'Blog_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL15.MSSQLSERVER\MSSQL\DATA\Blog_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
+ WITH CATALOG_COLLATION = DATABASE_DEFAULT
+GO
+ALTER DATABASE [Blog] SET COMPATIBILITY_LEVEL = 150
+GO
+IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
+begin
+EXEC [Blog].[dbo].[sp_fulltext_database] @action = 'enable'
+end
+GO
+ALTER DATABASE [Blog] SET ANSI_NULL_DEFAULT OFF 
+GO
+ALTER DATABASE [Blog] SET ANSI_NULLS OFF 
+GO
+ALTER DATABASE [Blog] SET ANSI_PADDING OFF 
+GO
+ALTER DATABASE [Blog] SET ANSI_WARNINGS OFF 
+GO
+ALTER DATABASE [Blog] SET ARITHABORT OFF 
+GO
+ALTER DATABASE [Blog] SET AUTO_CLOSE OFF 
+GO
+ALTER DATABASE [Blog] SET AUTO_SHRINK OFF 
+GO
+ALTER DATABASE [Blog] SET AUTO_UPDATE_STATISTICS ON 
+GO
+ALTER DATABASE [Blog] SET CURSOR_CLOSE_ON_COMMIT OFF 
+GO
+ALTER DATABASE [Blog] SET CURSOR_DEFAULT  GLOBAL 
+GO
+ALTER DATABASE [Blog] SET CONCAT_NULL_YIELDS_NULL OFF 
+GO
+ALTER DATABASE [Blog] SET NUMERIC_ROUNDABORT OFF 
+GO
+ALTER DATABASE [Blog] SET QUOTED_IDENTIFIER OFF 
+GO
+ALTER DATABASE [Blog] SET RECURSIVE_TRIGGERS OFF 
+GO
+ALTER DATABASE [Blog] SET  DISABLE_BROKER 
+GO
+ALTER DATABASE [Blog] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
+GO
+ALTER DATABASE [Blog] SET DATE_CORRELATION_OPTIMIZATION OFF 
+GO
+ALTER DATABASE [Blog] SET TRUSTWORTHY OFF 
+GO
+ALTER DATABASE [Blog] SET ALLOW_SNAPSHOT_ISOLATION OFF 
+GO
+ALTER DATABASE [Blog] SET PARAMETERIZATION SIMPLE 
+GO
+ALTER DATABASE [Blog] SET READ_COMMITTED_SNAPSHOT OFF 
+GO
+ALTER DATABASE [Blog] SET HONOR_BROKER_PRIORITY OFF 
+GO
+ALTER DATABASE [Blog] SET RECOVERY FULL 
+GO
+ALTER DATABASE [Blog] SET  MULTI_USER 
+GO
+ALTER DATABASE [Blog] SET PAGE_VERIFY CHECKSUM  
+GO
+ALTER DATABASE [Blog] SET DB_CHAINING OFF 
+GO
+ALTER DATABASE [Blog] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
+GO
+ALTER DATABASE [Blog] SET TARGET_RECOVERY_TIME = 60 SECONDS 
+GO
+ALTER DATABASE [Blog] SET DELAYED_DURABILITY = DISABLED 
+GO
+ALTER DATABASE [Blog] SET ACCELERATED_DATABASE_RECOVERY = OFF  
+GO
+EXEC sys.sp_db_vardecimal_storage_format N'Blog', N'ON'
+GO
+ALTER DATABASE [Blog] SET QUERY_STORE = OFF
+GO
+USE [Blog]
+GO
+ALTER DATABASE SCOPED CONFIGURATION SET IDENTITY_CACHE = OFF;
+GO
+USE [Blog]
+GO
+/****** Object:  Table [dbo].[ArticleComments]    Script Date: 4/15/2022 10:38:02 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[ArticleComments](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[ArticleId] [int] NOT NULL,
+	[CommentId] [int] NOT NULL,
+ CONSTRAINT [PK_ArticleComments] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Articles]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Articles](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [nvarchar](200) NOT NULL,
+	[DatePosted] [datetime2](7) NOT NULL,
+	[LastEdited] [datetime2](7) NULL,
+	[AuthorName] [nvarchar](110) NOT NULL,
+	[dbTags] [nvarchar](1000) NULL,
+	[ContentText] [nvarchar](max) NOT NULL,
+ CONSTRAINT [PK_Articles] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Comments]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Comments](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[DatePosted] [datetime2](7) NOT NULL,
+	[LastEdited] [datetime2](7) NULL,
+	[ContentText] [nvarchar](max) NOT NULL,
+	[AuthorId] [int] NOT NULL,
+ CONSTRAINT [PK_Comments] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[RefreshTokens]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[RefreshTokens](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[Token] [nvarchar](260) NOT NULL,
+	[Created] [datetime2](7) NOT NULL,
+	[CreatedByIp] [nvarchar](50) NOT NULL,
+	[Expires] [datetime2](7) NOT NULL,
+	[OwnerId] [int] NOT NULL,
+ CONSTRAINT [PK_RefreshTokens] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+/****** Object:  Table [dbo].[Users]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [dbo].[Users](
+	[id] [int] IDENTITY(1,1) NOT NULL,
+	[FirstName] [nvarchar](50) NOT NULL,
+	[LastName] [nvarchar](60) NOT NULL,
+	[EmailAddress] [nvarchar](200) NOT NULL,
+	[Role] [nvarchar](50) NOT NULL,
+	[PasswordHash] [nvarchar](200) NOT NULL,
+	[DoesReceiveNotifications] [bit] NOT NULL,
+ CONSTRAINT [PK_Users] PRIMARY KEY CLUSTERED 
+(
+	[id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+ALTER TABLE [dbo].[ArticleComments]  WITH CHECK ADD  CONSTRAINT [FK_ArticleComments_ArticleId] FOREIGN KEY([ArticleId])
+REFERENCES [dbo].[Articles] ([id])
+GO
+ALTER TABLE [dbo].[ArticleComments] CHECK CONSTRAINT [FK_ArticleComments_ArticleId]
+GO
+ALTER TABLE [dbo].[ArticleComments]  WITH CHECK ADD  CONSTRAINT [FK_ArticleComments_CommentId] FOREIGN KEY([CommentId])
+REFERENCES [dbo].[Comments] ([id])
+GO
+ALTER TABLE [dbo].[ArticleComments] CHECK CONSTRAINT [FK_ArticleComments_CommentId]
+GO
+ALTER TABLE [dbo].[Comments]  WITH CHECK ADD  CONSTRAINT [FK_Comments_AuthorId] FOREIGN KEY([AuthorId])
+REFERENCES [dbo].[Users] ([id])
+GO
+ALTER TABLE [dbo].[Comments] CHECK CONSTRAINT [FK_Comments_AuthorId]
+GO
+ALTER TABLE [dbo].[RefreshTokens]  WITH CHECK ADD  CONSTRAINT [FK_RefreshTokens_OwnerId] FOREIGN KEY([OwnerId])
+REFERENCES [dbo].[Users] ([id])
+GO
+ALTER TABLE [dbo].[RefreshTokens] CHECK CONSTRAINT [FK_RefreshTokens_OwnerId]
+GO
+/****** Object:  StoredProcedure [dbo].[spArticles_Delete]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spArticles_Delete]
+	@id int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	delete from dbo.Articles where id = @id;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spArticles_GetAll]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spArticles_GetAll]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	select * from dbo.Articles;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spArticles_GetById]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spArticles_GetById]
+	@id int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	select * from dbo.Articles where id = @id;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spArticles_Insert]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spArticles_Insert]
+	@Title nvarchar(200),
+	@DatePosted datetime2(7),
+	@AuthorName nvarchar(110),
+	@dbTags nvarchar(1000),
+	@ContentText nvarchar(max),
+	@id int = 0 output
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	insert into dbo.Articles (Title, DatePosted, AuthorName, dbTags, ContentText)
+	values (@Title, @DatePosted, @AuthorName, @dbTags, @ContentText)
+
+	select @id = SCOPE_IDENTITY();
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spArticles_Update]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spArticles_Update]
+	@id int,
+	@Title nvarchar(200),
+	@LastEdited datetime2(7),
+	@AuthorName nvarchar(110),
+	@dbTags nvarchar(1000),
+	@ContentText nvarchar(max)	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	update dbo.Articles 
+	set Title = @Title, LastEdited = @LastEdited, AuthorName = @AuthorName, dbTags = @dbTags, ContentText = @ContentText
+	where id = @id;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spComments_Delete]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spComments_Delete]
+	@id int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	delete from dbo.ArticleComments where CommentId = @id
+
+	delete from dbo.Comments where id = @id;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spComments_GetAll]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spComments_GetAll]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	select * from dbo.Comments;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spComments_GetByArticle]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spComments_GetByArticle]
+	@ArticleId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	select c.*
+	FROM dbo.Comments c
+	inner join dbo.ArticleComments a on c.id = a.CommentId
+	where a.ArticleId = @ArticleId
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spComments_Insert]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spComments_Insert]
+	@ArticleId int,
+	@DatePosted datetime2(7),
+	@ContentText nvarchar(max),
+	@AuthorId int,
+	@id int = 0 output
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	insert into dbo.Comments (DatePosted, ContentText, AuthorId)
+	values (@DatePosted, @ContentText, @AuthorId)
+
+	declare @commentId int;
+	set @commentId = SCOPE_IDENTITY();
+
+	insert into dbo.ArticleComments (ArticleId, CommentId)
+	values (@ArticleId, @commentId)
+
+	select @id = @commentId
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spComments_Update]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spComments_Update]
+	@id int,
+	@LastEdited datetime2(7),
+	@ContentText nvarchar(max)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	update dbo.Comments
+	set LastEdited = @LastEdited, ContentText = @ContentText
+	where id = @id;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spRefreshTokens_Delete]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[spRefreshTokens_Delete]
+	@id int	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	delete from dbo.RefreshTokens where id = @id;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spRefreshTokens_GetAll]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[spRefreshTokens_GetAll]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	select * from dbo.RefreshTokens;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spRefreshTokens_GetByUser]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[spRefreshTokens_GetByUser]
+	@OwnerId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	select * from dbo.RefreshTokens where OwnerId = @OwnerId;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spRefreshTokens_Insert]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[spRefreshTokens_Insert]
+	@Token nvarchar(260),
+	@Created datetime2(7),
+	@CreatedByIp nvarchar(50),
+	@Expires datetime2(7),
+	@OwnerId int,
+	@id int = 0 output
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	insert into dbo.RefreshTokens (Token, Created, CreatedByIp, Expires, OwnerId)
+	values (@Token, @Created, @CreatedByIp, @Expires, @OwnerId)
+	
+	select @id = SCOPE_IDENTITY();
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spUsers_Delete]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE PROCEDURE [dbo].[spUsers_Delete]
+	@id int	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	delete from dbo.Users where id = @id;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spUsers_GetAll]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spUsers_GetAll] 
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * from dbo.Users;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spUsers_GetById]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spUsers_GetById] 
+	@UserId int
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for procedure here
+	SELECT * from dbo.Users where id = @UserId;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spUsers_Insert]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spUsers_Insert]
+	@FirstName nvarchar(50),
+	@LastName nvarchar(60),
+	@EmailAddress nvarchar(200),
+	@Role nvarchar(50),
+	@PasswordHash nvarchar(200),
+	@DoesReceiveNotifications bit,
+	@id int = 0 output
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	insert into dbo.Users (FirstName, LastName, EmailAddress, Role, PasswordHash, DoesReceiveNotifications)
+	values (@FirstName, @LastName, @EmailAddress, @Role, @PasswordHash, @DoesReceiveNotifications)
+
+	select @id = SCOPE_IDENTITY();
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spUsers_Update]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spUsers_Update]
+	@id int,
+	@FirstName nvarchar(50),
+	@LastName nvarchar(60),
+	@EmailAddress nvarchar(200),
+	@Role nvarchar(50),
+	@DoesReceiveNotifications bit	
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	update dbo.Users
+	set FirstName = @FirstName, LastName = @LastName, EmailAddress = @EmailAddress, Role = @Role, DoesReceiveNotifications = @DoesReceiveNotifications
+	where id = @id;
+END
+GO
+/****** Object:  StoredProcedure [dbo].[spUsers_UpdatePassword]    Script Date: 4/15/2022 10:38:03 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE PROCEDURE [dbo].[spUsers_UpdatePassword]
+	@id int,
+	@PasswordHash nvarchar(200)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+	update dbo.Users
+	set PasswordHash = @PasswordHash
+	where id = @id;
+END
+GO
+USE [master]
+GO
+ALTER DATABASE [Blog] SET  READ_WRITE 
+GO
