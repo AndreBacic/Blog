5aafa8514bf5ba37473e5c9334461f27fe8c79a2
andre9050@yahoo.com <andre9050@yahoo.com>
2022-06-28 18:08:03 -0400

    Still can't get the API and React app to sync

---
+++ reacttsfrontend/src/setupProxy.js
@@ -0,0 +1,16 @@
+﻿const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/api/accountAPI",
+    "/api/articleAPI",
+    "/api/commentAPI",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'https://localhost:44325',
+        secure: false
+    });
+
+    app.use(appProxy);
+};
